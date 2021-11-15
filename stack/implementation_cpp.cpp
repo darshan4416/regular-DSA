4225b0d35720987c2c8fcb9d6fe884fab47ca560
darshan <darshanhirekurubar4416@gmail.com>
2021-11-15 23:53:47 +0530

    stack implementation added cpp

--- stack/implementation_cpp.cpp
+++ stack/implementation_cpp.cpp
@@ -1,12 +1,69 @@
 #include<iostream>
 #include<stack>
+#include<vector>
 using namespace std;
+
+class Stack{
+	
+	int top;
+	vector<int> vt;
+	
+	public:
+		
+	Stack(){
+	this->top = -1;
+	}	
+	void push(int x){
+		this->vt.push_back(x);
+		this->top++;
+	}
+	void pop(){
+		if(this->top==-1){
+			cout<<"Underflow"<<endl;
+		}
+		else{
+		this->vt.pop_back(),this->top--;	
+		} 	
+	}
+	int peek(){
+		if(this->top==-1){
+			cout<<"Empty Stack"<<endl;
+			return 0;
+		}
+		else{
+			return this->vt[top];
+		}
+		 
+	}
+	int size(){
+		return this->vt.size();
+	}
+	int empty(){
+		return this->vt.size() == 0 ? 1 : 0;
+	}
+};
 
 int main(){
 	
-	stack<int> st;
-	st.push(2020);
-	st.push(2021);
+	Stack st ;
+	st.push(5);
+	cout<<st.peek()<<endl;
+	
+	st.push(6);
+	cout<<st.peek()<<endl;
+	
+	st.push(7);
+	cout<<st.peek()<<endl;
+	st.push(8);
+	cout<<st.peek()<<endl;
+	
+	st.pop();
+	st.pop();
+	st.pop();
+	st.pop();
+	st.pop();
+	
+	cout<<st.peek()<<endl;
 	
 	
 	
