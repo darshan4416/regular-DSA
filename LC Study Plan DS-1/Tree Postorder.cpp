42a7bacfead398c8ad678cb85d2e8a6a998080d0
Darshan Hirekurubar <73415299+darshan4416@users.noreply.github.com>
2021-11-25 23:14:22 +0530

    Create Tree Postorder.cpp

---
+++ LC Study Plan DS-1/Tree Postorder.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+     vector<int> ans;
+    void postorder(TreeNode* node){
+        
+        if(node==NULL){
+            return ;
+        }
+        
+        postorder(node->left);
+        postorder(node->right);
+        ans.push_back(node->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        
+        postorder(root);
+        return ans;
+    }
